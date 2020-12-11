202442b8c7f55411403d2820fc7a4b4b61d0e18d
Niedson Santana <niedsonsantana@hotmail.com>
2020-12-10 21:47:27 -0300

    Projeto pronto

--- core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Animal
-
-
-
-class AnimalModelForm(forms.ModelForm):
-    class Meta:
-        model = Animal
-        fields=['nome', 'porte', 'cor', 'castrado', 'tipo', 'image', 'info']
